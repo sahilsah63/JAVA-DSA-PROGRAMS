8507e47f16758339976a392718efdda6e0c5b40c
SAHIL KUMAR SAH <sahilsah6387@gmail.com>
2024-06-15 12:13:17 +0530

    Add files via upload

---
+++ AddTwoNumbers.java
@@ -0,0 +1,8 @@
+public class AddTwoNumbers {
+    public static void main(String args[]){
+        int a = 50;
+        int b = 100;
+        int c = a+b;
+        System.out.println("Result is:"+c);
+    }
+}
