43ea58234d9a3d797d9d26dca1a71d081faa16f2
SAHIL KUMAR SAH <sahilsah6387@gmail.com>
2024-06-20 10:28:02 +0530

    Push operation in Stack

---
+++ StackMain.java
@@ -0,0 +1,10 @@
+import java.util.Stack;
+public class StackMain {
+   public static void main(String args[]){
+    Stack<String> mystack=new Stack<String>();
+    mystack.push("JAVA");
+    mystack.push("PYTHON");
+    mystack.push("c++");
+    System.out.println("Stack Elements="+mystack);
+   } 
+}
