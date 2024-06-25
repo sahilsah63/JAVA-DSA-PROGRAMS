c75e70db72e8336349cf1d2bd3dd3bcad6a0be91
SAHIL KUMAR SAH <sahilsah6387@gmail.com>
2024-06-25 12:52:51 +0530

    Update LinkList.java

--- LinkList.java
+++ LinkList.java
@@ -5,7 +5,9 @@ public class LinkList {
         name.add("CAR");
         name.add("BIKE");
         name.add("PHONE");
+        name.add("BULLET");
         System.out.println(name);
+        //TO REMOVE THE ELEMENT FROM LINKEDLIST.....
         name.remove("PHONE");
         //BELOW LINE IS FOR PRINT ALL ELEMENTS IN NEXT LINE.....
         for(String X : name){
